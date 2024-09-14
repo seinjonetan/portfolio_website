885687a4bb9af57bd52de87c78ef83e039a0fa15
Sein Jone <seinjonetan@gmail.com>
2024-09-13 22:50:17 -0700

    chore: static elements

---
+++ src/static/skillsData.ts
@@ -0,0 +1,51 @@
+export const skillsData = [
+    {
+      name: "Programming Languages",
+      skills: [
+        {
+          name: "JavaScript",
+          link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        },
+        { name: "Python", link: "https://www.python.org/" },
+        { name: "R", link: "https://www.r-project.org/" },
+        { name: "SQL", link: "https://www.w3schools.com/sql/" },
+        { name: "Julia", link: "https://julialang.org/" },
+        { name: "MATLAB", link: "https://www.mathworks.com/products/matlab.html" },
+        { name: "C++", link: "https://www.cplusplus.com/" },
+      ],
+    },
+    {
+      name: "Web Technologies",
+      skills: [
+        { name: "React", link: "https://reactjs.org/" },
+        { name: "Next.js", link: "https://nextjs.org/" },
+      ],
+    },
+    {
+      name: "Tools & Platforms",
+      skills: [
+        { name: "Git", link: "https://git-scm.com/" },
+        { name: "Docker", link: "https://www.docker.com/" },
+        { name: "Proxmox", link: "https://www.proxmox.com/" },
+      ],
+    },
+    {
+        name: "Data Analysis",
+        skills: [
+          { name: "Pandas", link: "https://pandas.pydata.org/" },
+          { name: "Numpy", link: "https://numpy.org/" },
+          { name: "Scikit-learn", link: "https://scikit-learn.org/stable/" },
+          { name: "Statsmodels", link: "https://www.statsmodels.org/stable/index.html" },
+          { name: "Polars", link: "pola.rs" },
+          { name: "Dask", link: "https://dask.org/" },
+        ],
+    },
+    {
+        name: "Machine Learning",
+        skills: [
+          { name: "TensorFlow", link: "https://www.tensorflow.org/" },
+          { name: "PyTorch", link: "https://pytorch.org/" },
+          { name: "transformers", link: "https://huggingface.co/transformers/" },
+        ],
+    }
+  ];
