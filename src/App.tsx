8e26b4e33ac8f6f6287cfffabd235e3f7578b032
Sein Jone <seinjonetan@gmail.com>
2024-09-13 20:36:02 -0700

    feat: app routing and wrappers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { CssVarsProvider } from "@mui/joy";
+import { ThemeProvider } from "./ThemeProvider"; // Import the ThemeProvider
+import "@mantine/core/styles.css";
+import HomePage from "./pages/HomePage";
+// import InvoicePage from "./pages/InvoicePage";
+// import ScraperPage from "./pages/ScraperPage";
+// import NewsTempPage from "./pages/NewsTempPage";
+// import ResearchPage from "./pages/ResearchPage";
 
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <ThemeProvider>
+      {" "}
+      {/* Wrap the application with ThemeProvider */}
+      <MantineProvider>
+        <CssVarsProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* <Route path="/invoice" element={<InvoicePage />} />
+              <Route path="/scrape" element={<ScraperPage />} />
+              <Route path="/news" element={<NewsTempPage />} />
+              <Route path="/research" element={<ResearchPage />} /> */}
+            </Routes>
+          </Router>
+        </CssVarsProvider>
+      </MantineProvider>
+    </ThemeProvider>
   );
 }
 
